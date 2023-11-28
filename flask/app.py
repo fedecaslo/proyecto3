1d8da5fddb7c001457c3c7969ab8e937b40d8d8c
luciaml02 <lucia.martinez.lpez@gmail.com>
2023-11-28 15:35:47 +0100

    Kafka y csv para flask

--- flask/app.py
+++ flask/app.py
@@ -1,32 +1,50 @@
-from flask import Flask, request
+from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from confluent_kafka import Producer
 import csv
 import os
+import subprocess
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./plantillas')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'file' not in request.files:
         return 'No file part'
+
     file = request.files['file']
+
     if file.filename == '':
         return 'No selected file'
+
     if file and file.filename.endswith('.csv'):
         filename = secure_filename(file.filename)
-        file.save(filename)
-        send_to_kafka(filename)
-        return 'File uploaded and sent to Kafka'
+        file_path = os.path.abspath(os.path.join('csvs_for_upload', filename))
+        os.makedirs('./csvs_for_upload', exist_ok=True)
+        subprocess.run(['chmod', '755', './csvs_for_upload'])
+        file.save(file_path)
+        
+        try:
+            print(f'File "{filename}" uploaded to Flask')
+            send_to_kafka(file_path)
+            return 'File uploaded and sent to Kafka'
+        except Exception as e:
+            return f'Error: {str(e)}'
+        finally:
+            os.remove(file_path)
     else:
         return 'Invalid file type'
 
 def send_to_kafka(filename):
-    producer = Producer({'bootstrap.servers': 'localhost:9092'})
+    producer = Producer({'bootstrap.servers': 'kafka:9092'})
     with open(filename, newline='') as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
-            producer.produce('mytopic', ','.join(row))
+            producer.produce('csv_upload', ','.join(row))
     producer.flush()
     os.remove(filename)
 
